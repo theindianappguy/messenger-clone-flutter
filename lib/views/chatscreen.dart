577569296b7adfa403a0cab91dbae3374e0cecf1
theindianappguy <theindianappguy@gmail.com>
2021-01-12 17:05:57 +0530

    long message bug fixed

--- lib/views/chatscreen.dart
+++ lib/views/chatscreen.dart
@@ -78,23 +78,26 @@ class _ChatScreenState extends State<ChatScreen> {
       mainAxisAlignment:
           sendByMe ? MainAxisAlignment.end : MainAxisAlignment.start,
       children: [
-        Container(
-            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(24),
-                bottomRight:
-                    sendByMe ? Radius.circular(0) : Radius.circular(24),
-                topRight: Radius.circular(24),
-                bottomLeft: sendByMe ? Radius.circular(24) : Radius.circular(0),
+        Flexible(
+          child: Container(
+              margin: EdgeInsets.symmetric(horizontal: 16, vertical: 4),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(24),
+                  bottomRight:
+                      sendByMe ? Radius.circular(0) : Radius.circular(24),
+                  topRight: Radius.circular(24),
+                  bottomLeft:
+                      sendByMe ? Radius.circular(24) : Radius.circular(0),
+                ),
+                color: sendByMe ? Colors.blue : Color(0xfff1f0f0),
               ),
-              color: Colors.blue,
-            ),
-            padding: EdgeInsets.all(16),
-            child: Text(
-              message,
-              style: TextStyle(color: Colors.white),
-            )),
+              padding: EdgeInsets.all(16),
+              child: Text(
+                message,
+                style: TextStyle(color: Colors.white),
+              )),
+        ),
       ],
     );
   }
